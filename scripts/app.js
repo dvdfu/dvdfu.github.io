e71340cfd8964a8cadb19f8b909fe88442a60b20
dvdfu <davidf1212@gmail.com>
2017-09-10 21:34:06 -0400

    nuke it

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Container = require('./container');
-
-window.addEventListener('hashchange', function() {
-  scrollBy(0, -50);
-});
-
-ReactDOM.render(<Container/>, document.getElementById('container'));
--- scripts/project-tools.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var ProjectTools = React.createClass({
-  render: function() {
-    return (<h5>{this.props.tools.join(', ')}</h5>);
-  }
-});
-
-module.exports = ProjectTools;
